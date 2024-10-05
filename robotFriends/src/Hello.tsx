e956b30ff892473334deaf905f89a6a04508f5e8
bmalvo <boydmalvo@gmail.com>
2024-10-05 12:16:44 +0200

    init project and add component Hello

---
+++ robotFriends/src/Hello.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import './Hello.css'
+
+export class Hello extends Component {
+
+    render() {
+        
+        return <>
+            <h1>Hello world!</h1>
+            <p>Welcome to React</p>
+        </>
+    }
+}
