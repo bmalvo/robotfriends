e2e50cc3180b3cbc4ea74921fc6ff29110b1b56f
bmalvo <boydmalvo@gmail.com>
2024-10-05 14:18:57 +0200

    cleaning before robofriends project

--- robotFriends/src/main.tsx
+++ robotFriends/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import { Hello } from './Hello.tsx'
 import 'tachyons'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Hello greetings={ "Ninja"} />
-    <App />
+    
   </StrictMode>,
 )
