6ca317e34a810579732bd78dadd5482d9d1a0471
bmalvo <boydmalvo@gmail.com>
2024-10-05 15:11:09 +0200

    destructuring robots using map

--- robotFriends/src/Card.tsx
+++ robotFriends/src/Card.tsx
@@ -10,7 +10,7 @@ export const Card = ({ id, name, email}: Robots) => {
     return <>
         <div className=" tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
         <div>
-                <img src={`https://robohash.org/${id}?200x200`} alt="pic of robot" />
+                <img src={`https://robohash.org/${id}?200x200?`} alt="pic of robot" />
         </div>
         <div>
                 <h2>{ name}</h2>
