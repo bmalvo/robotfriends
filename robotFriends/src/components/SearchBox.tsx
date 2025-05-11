cca69fe7f43f4fd1249650d06db054924cb3f27e
bmalvo <boydmalvo@gmail.com>
2025-05-11 08:14:35 +0200

    code review -> SearchBox.tsx

--- robotFriends/src/components/SearchBox.tsx
+++ robotFriends/src/components/SearchBox.tsx
@@ -1,17 +1,17 @@
 interface SearchBoxProps {
     searchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
+};
 
 export const SearchBox: React.FC<SearchBoxProps> = ({ searchChange }) => {
 
-    return <>
-        <div className="pa2">
-          <input
-            className="pa3 ba b--green bg-lightest-blue"
-            type="search"
-            placeholder="search robot"
-            onChange={searchChange}    
-          />
-        </div>
-    </>
-}
+  return <>
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robot"
+        onChange={searchChange}
+      />
+    </div>
+  </>
+};
